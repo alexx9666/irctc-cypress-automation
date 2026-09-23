821489c98d6a65c739abccb519255f720fd7c7ca
alexx9666 <147835003+alexx9666@users.noreply.github.com>
2026-09-23 13:07:35 +0530

    Configure Chromium launch options in Cypress
    Added browser launch options for Chromium in Cypress.

--- cypress.config.js
+++ cypress.config.js
@@ -8,11 +8,16 @@ module.exports = defineConfig({
 
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.family === 'chromium') {
+          launchOptions.args.push('--disable-gpu');
+          launchOptions.args.push('--no-sandbox');
+          launchOptions.args.push('--disable-dev-shm-usage');
+        }
+        return launchOptions;
+      });
       on('task', {
         log(message) {
-          // Then to see the log messages in the terminal
-          //   cy.task("log", "my message");
           console.log(message + '\n\n');
           return null;
         },
